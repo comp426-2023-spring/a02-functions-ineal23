90f5f6f2f04029f33954b87223a30dfae83ddfa7
Isaiah Neal <ineal23@email.unc.edu>
2023-03-23 20:33:25 -0400

    Added log file.

--- cli.js
+++ cli.js
@@ -1,30 +1,26 @@
 #!/usr/bin/env node
-import minimist from 'minimist';
-import moment from 'moment-timezone';
-import fetch from 'node-fetch';
+import minimist from "minimist";
+import moment from "moment-timezone";
+import fetch from "node-fetch";
 
 const args = minimist(process.argv.slice(2));
 
-if (args.h) {
-  console.log(`Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+const help_str = `Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
     -h            Show this help message and exit.
     -n, -s        Latitude: N positive; S negative.
     -e, -w        Longitude: E positive; W negative.
     -z            Time zone: uses tz.guess() from moment-timezone by default.
     -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
-    -j            Echo pretty JSON from open-meteo API and exit.`);
-  process.exit(0);
-}
+    -j            Echo pretty JSON from open-meteo API and exit.`;
 
 const help = args.h;
-const timezone = args.z ? args.z : moment.tz.guess();
-const lat = args.n ? parseFloat(args.n).toFixed(2) : (args.s ? parseFloat(args.s * -1).toFixed(2) : null);
-const lg = args.e ? parseFloat(args.e).toFixed(2) : (args.w ? parseFloat(args.w * -1).toFixed(2) : null);
+const timezone = args.z || moment.tz.guess();
+const lat = args.n || args.s * -1;
+const lg = args.e || args.w * -1;
 const json = args.j;
-const day = args.d ? parseInt(args.d) : 1;
+const day = args.d === undefined ? 1 : args.d;
 
 const URL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lg}&daily=precipitation_hours&current_weather=true&timezone=${timezone}`;
-
 fetch(URL)
   .then(response => response.json())
   .then(data => {
@@ -38,7 +34,7 @@ fetch(URL)
   });
 
 function weather(data) {
-  const date_log = day === 0 ? " today." : (day === 1 ? " tomorrow." : ` in ${day} days.`);
+  const date_log = day == 0 ? " today." : (day == 1 ? " tomorrow." : ` in ${day} days.`);
   const precip = data.daily.precipitation_hours;
   const message = precip[day] >= 1 ? "You might need your galoshes" : "You will not need your galoshes";
   console.log(`${message}${date_log}`);
